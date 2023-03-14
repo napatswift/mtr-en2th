5b8769ddedb0a032a8a6e320095e8d15257964b0
napatswift <napat.do@ku.th>
2023-03-14 23:26:33 +0700

    Add sentencepieces train file

---
+++ sp-train.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sentencepiece as spm
+
+def create_temp_file() -> dict:
+  dataset_df = pd.read_csv('dataset/translate_all.csv')
+  temp_files = {}
+  for lang in ['th', 'en']:
+    sentence_lines = dataset_df[lang].tolist()
+    total_line_count = 0
+    fname = f'dataset/.lines_{lang.lower()}.txt'
+    temp_files[lang] = fname
+    with open(fname, 'w') as f:
+      string_lines = [str(l) for l in sentence_lines]
+      for l in string_lines:
+        f.write(l)
+        f.write('\n')
+        total_line_count += 1
+      print('total_line_count', total_line_count)
+  return temp_files
+
+def build_spmodel(lang, fpath):
+  spm.SentencePieceTrainer.train(
+    input=fpath,
+    model_prefix=f'spmodel/{lang}',
+    vocab_size=1000,
+    pad_id=3)
+
+
+if __name__ == '__main__':
+  for lang, fpath in create_temp_file().items():
+    build_spmodel(lang, fpath)
